e71fb1edb5debc1f9aad329202d5503213a1e750
UsernameTakenOof <91937816+UsernameTakenOof@users.noreply.github.com>
2021-11-25 00:11:49 +0100

    Finished Aufgabe 4

--- Aufgabe4/script.js
+++ Aufgabe4/script.js
@@ -1,15 +1,14 @@
+"use strict";
 // Simple counter to track EventPlanner elements
-let id: number = 0;
-
+let id = 0;
 class EventPlanner {
     // Data
-    private interpret: string;
-    private date: string;
-    private price: string;
-    private id: number;
-
+    interpret;
+    date;
+    price;
+    id;
     // Constructor also gives each element an ID 
-    constructor(i: string, d: string, p: string) {
+    constructor(i, d, p) {
         this.interpret = i;
         this.date = d;
         this.price = p;
@@ -17,98 +16,84 @@ class EventPlanner {
         id++;
         console.log(this.asString());
     }
-
     // Getters
-    asString(): string {
+    asString() {
         return `${this.interpret} kostet ${this.price} und findet am ${this.date} statt. id: ${this.id}`;
     }
-
-    getInterpret(): string {
+    getInterpret() {
         return this.interpret;
     }
-
-    getDate(): string {
+    getDate() {
         return this.date;
     }
-
-    getPrice(): string {
+    getPrice() {
         return this.price;
     }
-
-    getID(): string {
+    getID() {
         return this.id.toString();
     }
 }
-
 // Input variables
-let interpretinput: HTMLInputElement = document.getElementById("cinterpret") as HTMLInputElement;
-let priceinput: HTMLInputElement = document.getElementById("cprice") as HTMLInputElement;
-let dateinput: HTMLInputElement = document.getElementById("cdate") as HTMLInputElement;
-
+let interpretinput = document.getElementById("cinterpret");
+let priceinput = document.getElementById("cprice");
+let dateinput = document.getElementById("cdate");
 // Event related variables / listeners
-let events: EventPlanner[] = [];
-let addButton: HTMLElement = document.getElementById("formbutton");
+let events = [];
+let addButton = document.getElementById("formbutton");
 addButton.addEventListener("click", addEvent);
-
 // Create new event and add it to the events array. Call helper function to display in HTML document
-function addEvent(): void {
+function addEvent() {
     // Check whether input fields are empty
     if (interpretinput.value == "" || dateinput.value == "" || priceinput.value == "") {
         alert("Bitte alle Felder ausfüllen!");
         return;
     }
     // Create the event
-    let entry: EventPlanner = new EventPlanner(interpretinput.value, dateinput.value, priceinput.value)
+    let entry = new EventPlanner(interpretinput.value, dateinput.value, priceinput.value);
     events.push(entry);
     addTableEntry(entry);
-
     //Clear input fields. Commented out for easier testing.
     //interpretinput.value = "";
     //dateinput.value = "";
     //priceinput.value = "";
 }
-
 // Remove event from HTML table
-function removeTableEntry(event: Event): void {
-    let element: HTMLElement = <HTMLElement>event.currentTarget;
-    let parent: HTMLElement = ( <HTMLElement>event.target ).parentElement;
+function removeTableEntry(event) {
+    let element = event.currentTarget;
+    let parent = event.target.parentElement;
     removeEvent(element.getAttribute("data-id"));
     parent.remove();
 }
-
 // Remove event from events array
-function removeEvent(itemid: string): void {
-    events.forEach( (eventelem, index) => {
-        if (eventelem.getID() == itemid) events.splice(index, 1);
+function removeEvent(itemid) {
+    events.forEach((eventelem, index) => {
+        if (eventelem.getID() == itemid)
+            events.splice(index, 1);
     });
     console.log(events);
 }
-
 // Create new HTML table elements to display data
-function addTableEntry(eventitem: EventPlanner): void {
+function addTableEntry(eventitem) {
     // Create HTML elements
-    let entry: HTMLTableRowElement =      document.createElement("tr");
-    let date: HTMLTableCellElement =      document.createElement("td");
-    let interpret: HTMLTableCellElement = document.createElement("td");
-    let price: HTMLTableCellElement =     document.createElement("td");
-    let trash: HTMLTableCellElement =     document.createElement("td");
-
+    let entry = document.createElement("tr");
+    let date = document.createElement("td");
+    let interpret = document.createElement("td");
+    let price = document.createElement("td");
+    let trash = document.createElement("td");
     // Data collection
     date.innerHTML = eventitem.getDate();
     interpret.innerHTML = eventitem.getInterpret();
     price.innerHTML = eventitem.getPrice();
-
     // Add trash functionality
     trash.setAttribute("class", "trash");
     trash.setAttribute("data-id", eventitem.getID());
     trash.addEventListener("click", removeTableEntry);
-
     // Build HTML table row element
     entry.appendChild(date);
     entry.appendChild(interpret);
     entry.appendChild(price);
     entry.appendChild(trash);
-
     // Display on website
     document.getElementById("table2").appendChild(entry);
 }
+//# sourceMappingURL=script.js.map
